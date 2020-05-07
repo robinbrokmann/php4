f1fe08089dc65b74f63385d46af1776f4eefb583
robin <106778@student.drenthecollege.nl>
2020-05-07 09:03:53 +0200

    opdracht1 klaar

--- php/includes/default.inc.php
+++ php/includes/default.inc.php
@@ -1,6 +1,6 @@
 <?php
   //constants for including files
-  define("WEB_ROOT", DIRECTORY_SEPARATOR . "OOP_template" . DIRECTORY_SEPARATOR);
+  define("WEB_ROOT", DIRECTORY_SEPARATOR . "php4" . DIRECTORY_SEPARATOR . "mvc" . DIRECTORY_SEPARATOR);
   define("LOCAL_ROOT", $_SERVER['DOCUMENT_ROOT'] . WEB_ROOT);
 
   //function for loading class files inclusive namespaces
